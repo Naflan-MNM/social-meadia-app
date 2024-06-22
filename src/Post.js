ad6acc8531d624039eddc166195bba5ccf4fc321
Naflan-MNM <naflanm084@gmail.com>
2024-06-23 00:40:35 +0530

    navigate logic to move postpage

--- src/Post.js
+++ src/Post.js
@@ -1,13 +1,17 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate  } from 'react-router-dom'
 
 const Post = ({ post}) => {
-    
+  const navigate = useNavigate();//you must call this useNavigate function at the top, not a inside the function
+  const handleClick = ()=>{
+    navigate ('postpage')
+      
+    }
   return (
     <article className="post">
-      <h1>{post.title}</h1>
+     <h1 onClick={()=>handleClick()}>{post.title}</h1>
       <p className='postDate'>{post.datetime}</p>
-      <p className='postBody'>{(post.body).length <=25 ? post.body : `${post.body.substring(0,25)}...`}</p>
+      <p onClick={()=>handleClick()}className='postBody'>{(post.body).length <=25 ? post.body : `${post.body.substring(0,25)}...`}</p>
     </article>
   )
 }
